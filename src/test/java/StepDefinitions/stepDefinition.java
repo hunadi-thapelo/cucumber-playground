50b36d50b83758cb83577488aebcc4ce296ec772
hunadim <hmapulane@gmail.com>
2022-12-19 18:07:29 +0200

    Working with parameterization for dara driven testing

--- src/test/java/StepDefinitions/stepDefinition.java
+++ src/test/java/StepDefinitions/stepDefinition.java
@@ -10,29 +10,34 @@ import java.util.List;
 public class stepDefinition {
     @Given("User is on Risk Input home page")
     public void User_is_on_risk_input_home_page() {
-        System.out.println("User is on the login page");
+        System.out.println("User is on Risk Input login page");
     }
-    @When("User logins in with valid {string} and {string}")
-    public void user_logins_in_with_valid_and(String string, String string2) {
-        System.out.println(string);
-        System.out.println(string2);
+    @When("User logs in with valid {string} and {string}")
+    public void user_logs_in_with_valid_and(String string, String string2)  {
+        System.out.println("Logged in with valid username: " +string);
+        System.out.println("Logged in with valid password: " +string2);
     }
     @Then("User is logged in successfully")
     public void user_is_logged_in_successfully() {
-        System.out.println("User is logged in");
+        System.out.println("User is logged in successfully. End of E2E test");
     }
     @Then("User domain name displayed {string}")
     public void user_domain_name_displayed(String string) {
-        System.out.println("Domain name is displayed is "+string);
+        System.out.println("Logged in. Domain name is displayed is "+string);
     }
     @When("User signs up with valid details")
     public void user_signs_up_with_valid_details(DataTable dataTable)//DataTable type parameter for multiple data we're working with
     {
         List<List<String>> credList = dataTable.asLists();
-        System.out.println(credList.get(0).get(0));//1st get represents row (in example we're working with one row), 2nd get is column number
-        System.out.println(credList.get(0).get(1));
-        System.out.println(credList.get(0).get(2));
-        System.out.println(credList.get(0).get(3));
+        System.out.println("Logged in with valid name: " +credList.get(0).get(0));//1st get represents row (in example we're working with one row), 2nd get is column number
+        System.out.println("Logged in with valid lastname: " +credList.get(0).get(1));
+        System.out.println("Logged in with valid email: " +credList.get(0).get(2));
+        System.out.println("Logged in with valid password: " +credList.get(0).get(3));
     }
+    @When("User logs into account with valid {string} and {string}")
+    public void user_logs_into_account_with_valid_and(String string, String string2) {
+        System.out.println("User logs into account with "+string+ " and "+string2+" !");
+    }
+
 }
 
--- src/test/resources/login.feature
+++ src/test/resources/login.feature
@@ -3,13 +3,13 @@ Feature: Application login
 
   Scenario: Home page login
     Given User is on Risk Input home page
-    When User logins in with valid "nala" and "passwOrd"
+    When User logs in with valid "nala" and "passwOrd"
     Then User is logged in successfully
     Then User domain name displayed "true"
 
   Scenario: Home page login
     Given User is on Risk Input home page
-    When User logins in with valid "penny" and "p@ssword"
+    When User logs in with valid "penny" and "p@ssword"
     Then User is logged in successfully
     Then User domain name displayed "false"
 
@@ -19,3 +19,17 @@ Feature: Application login
     | sophy | marlboro | s.malboro@email.com | smP@word |
     Then User is logged in successfully
     Then User domain name displayed "true"
+
+  Scenario Outline: Home page login
+    Given User is on Risk Input home page
+    When User logs into account with valid "<userName>" and "<passWord>"
+    Then User is logged in successfully
+    Then User domain name displayed "true"
+
+    Examples:
+    |userName|passWord|
+    |user1    |p@sw0rd1 |
+    |user2    |p@sw0rd2 |
+    |user3    |p@sw0rd3 |
+    |user4    |p@sw0rd4 |
+    |user5    |p@sw0rd5 |
